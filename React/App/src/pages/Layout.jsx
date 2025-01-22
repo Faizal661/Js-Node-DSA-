58103659679f290320a02cf3ce85b6ab15fe1f52
faizal <faisalt661@gmail.com>
2025-01-22 17:26:27 +0530

    to do react practiced

--- React/App/src/pages/Layout.jsx
+++ React/App/src/pages/Layout.jsx
@@ -3,20 +3,20 @@ import { Link, Outlet } from "react-router-dom";
 
 const Layout = () => {
     return (
-        <div className="flex h-screen ">
+        <div className="flex h-fit  ">
           <div className="border-black border-2 m-1 p-2 w-fit pr-12 pt-3 flex flex-col font-serif font-bold gap-1 rounded-md">
             <Link to="/useEffect.example">
-              <button>UseEffect example</button>
+              <button>UseEffect example</button><hr />
             </Link>
             <Link to="/To-do">
-              <button> To do list </button>
+              <button> To do list </button><hr />
             </Link>
             <Link to="/useEffect.example">
               <button> example 3</button>
             </Link>
           </div>
 
-          <div className="border-black border-2 m-1 p-1  pr-12 flex-1 font-mono font-medium gap-1 rounded-md">
+          <div className="border-black border-2 m-1 p-1  pr-12 pb-16 flex-1 font-mono font-medium gap-1 rounded-md">
             <Outlet />
           </div>
         </div>
--- React/App/src/pages/Todo.jsx
+++ React/App/src/pages/Todo.jsx
@@ -1,11 +1,44 @@
-import React from 'react'
+import React, { useRef, useState } from "react";
 
 const Todo = () => {
+  const [items, setItems] = useState([]);
+  const [inputVal, setInputVal] = useState("");
+  const handleSubmit = () => {
+    const item = { data: inputVal, id: new Date().toString(), done: false };
+    setItems((prev) => [...prev, item]);
+    console.log(items);
+    setInputVal("");
+  };
   return (
     <div>
-        
-    </div>
-  )
-}
+      <div className=" flex justify-center items-center h-52">
+        <label htmlFor="todo">Add New : </label>
+        <input
+          type="text"
+          id="todo"
+          value={inputVal}
+          onChange={(e) => setInputVal(e.target.value)}
+          className="border-2 ml-2 border-black p-1 rounded-md"
+        />
+        <button
+          onClick={handleSubmit}
+          className="border-2 ml-2 border-black p-1 rounded-md"
+        >
+          Add
+        </button>
+      </div>
 
-export default Todo
+      <div className="flex flex-col  h-96 w-96 m-auto overflow-y-auto gap-2 border-black border-2 rounded-md">
+        {items.map((item,i) => (
+          <div key={item.id} className="flex justify-around">
+            <p>{i+1}</p>
+            <p>{item.data}</p>
+            <p className="font-serif">{item.done ? 'done' :'not completed'}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Todo;
